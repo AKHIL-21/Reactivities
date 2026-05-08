90a88abf1a18013dedd1d65161917c7f0fec9aa5
AKHIL-21 <muthyala.akhil21@gmail.com>
2026-05-08 09:29:40 -0400

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,7 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(
     opt => opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnecton"))
 );
+builder.Services.AddCors();
 var app = builder.Build();
 
 using var scope =app.Services.CreateScope();
@@ -27,7 +28,7 @@ var logger = services.GetRequiredService<ILogger<Program>>();
 logger.LogError(ex,"An error occured during Migration.");
     
 }
-
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000","https://localhost:3000"));
 app.MapControllers();
 
 app.Run();
---
+++ Client/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import mkcert from 'vite-plugin-mkcert'
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': {
+        target: 'https://localhost:5001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  plugins: [
+    react(), mkcert(),
+    babel({ presets: [reactCompilerPreset()] })
+  ],
+})
